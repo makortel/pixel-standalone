7040d59fb197d22b1df55c7b718bf2cc2ac4b868
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-01-06 21:14:54 +0100

    Make main+analyzer split for naive CPU

--- main_naive.cc
+++ main_naive.cc
@@ -1,51 +1,23 @@
-#include <chrono>
 #include <cstring>
 #include <iostream>
 #include <memory>
 
+#include "analyzer_naive.h"
 #include "input.h"
 #include "modules.h"
 #include "output.h"
-#include "rawtodigi_naive.h"
-
-namespace {
-  constexpr int NLOOPS = 100;
-}
 
 int main(int argc, char **argv) {
   Input input = read_input();
   std::cout << "Got " << input.cablingMap.size << " for cabling, wordCounter " << input.wordCounter << std::endl;
 
-  int totaltime = 0;
+  double totaltime = 0;
 
   std::unique_ptr<Output> output;
-  for (int i = 0; i < NLOOPS; ++i) {
-    output = std::make_unique<Output>();
+  naive::analyze(input, output, totaltime);
 
-    auto start = std::chrono::high_resolution_clock::now();
-    naive::rawtodigi(&input.cablingMap,
-                     input.wordCounter,
-                     input.word,
-                     input.fedId,
-                     output->xx,
-                     output->yy,
-                     output->adc,
-                     output->digi,
-                     output->rawIdArr,
-                     output->moduleInd,
-                     &output->err,
-                     true,
-                     true,
-                     false);
-    auto stop = std::chrono::high_resolution_clock::now();
-
-    auto diff = stop - start;
-    auto time = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
-    totaltime += time;
-  }
-
-  std::cout << "Output: " << countModules(output->moduleInd, input.wordCounter) << " modules in "
-            << (static_cast<double>(totaltime) / NLOOPS) << " us" << std::endl;
+  std::cout << "Output: " << countModules(output->moduleInd, input.wordCounter) << " modules in " << totaltime << " us"
+            << std::endl;
 
   return 0;
 }
