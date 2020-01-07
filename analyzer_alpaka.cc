62e971301a692ca1b22d84eb61dd5c60d8f8d715
Andrea Bocci <andrea.bocci@cern.ch>
2020-01-07 13:43:23 +0100

    Minor cleanup of Alpaka code

--- analyzer_alpaka.cc
+++ analyzer_alpaka.cc
@@ -15,44 +15,42 @@ namespace {
 namespace ALPAKA_ARCHITECTURE {
 
   void analyze(Input const& input, Output& output, double& totaltime) {
-    DevHost const devHost(alpaka::pltf::getDevByIdx<PltfHost>(0u));
-    DevAcc const devAcc(alpaka::pltf::getDevByIdx<PltfAcc>(0u));
-    Idx const elements(1);
-    Vec const extent(elements);
+    const DevHost host(alpaka::pltf::getDevByIdx<PltfHost>(0u));
+    const DevAcc device(alpaka::pltf::getDevByIdx<PltfAcc>(0u));
+    const Vec size(1u);
 
-    Queue queue(devAcc);
+    Queue queue(device);
 
     totaltime = 0.;
 
     for (int i = 0; i < NLOOPS; i++) {
       using ViewInput = alpaka::mem::view::ViewPlainPtr<DevHost, const Input, Dim, Idx>;
-      ViewInput input_hBuf(&input, devHost, extent);
+      ViewInput input_hBuf(&input, host, size);
 
-      using BufDevInput = alpaka::mem::buf::Buf<DevAcc, Input, Dim, Idx>;
-      BufDevInput input_dBuf(alpaka::mem::buf::alloc<Input, Idx>(devAcc, extent));
+      auto input_dBuf(alpaka::mem::buf::alloc<Input, Idx>(device, size));
+      Input* input_d = alpaka::mem::view::getPtrNative(input_dBuf);
 
       using ViewOutput = alpaka::mem::view::ViewPlainPtr<DevHost, Output, Dim, Idx>;
-      ViewOutput output_hBuf(&output, devHost, extent);
+      ViewOutput output_hBuf(&output, host, size);
 
-      using BufDevOutput = alpaka::mem::buf::Buf<DevAcc, Output, Dim, Idx>;
-      BufDevOutput output_dBuf(alpaka::mem::buf::alloc<Output, Idx>(devAcc, extent));
-      alpaka::mem::view::getPtrNative(output_hBuf)
-          ->err.construct(pixelgpudetails::MAX_FED_WORDS, alpaka::mem::view::getPtrNative(output_dBuf)->err_d);
+      auto output_dBuf(alpaka::mem::buf::alloc<Output, Idx>(device, size));
+      Output* output_d = alpaka::mem::view::getPtrNative(output_dBuf);
+      output.err.construct(pixelgpudetails::MAX_FED_WORDS, output_d->err_d);
 
       auto start = std::chrono::high_resolution_clock::now();
 
-      alpaka::mem::view::copy(queue, input_dBuf, input_hBuf, extent);
-      alpaka::mem::view::copy(queue, output_dBuf, output_hBuf, extent);
+      alpaka::mem::view::copy(queue, input_dBuf, input_hBuf, size);
+      alpaka::mem::view::copy(queue, output_dBuf, output_hBuf, size);
 
-      rawtodigi(alpaka::mem::view::getPtrNative(input_dBuf),
-                alpaka::mem::view::getPtrNative(output_dBuf),
+      rawtodigi(input_d,
+                output_d,
                 input.wordCounter,
                 true,
                 true,
                 false,
                 queue);
 
-      alpaka::mem::view::copy(queue, output_hBuf, output_dBuf, extent);
+      alpaka::mem::view::copy(queue, output_hBuf, output_dBuf, size);
 
       alpaka::wait::wait(queue);
 
