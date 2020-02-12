6d7410baa97e5596b655486fb75a6acd74b32d96
Andrea Bocci <andrea.bocci@cern.ch>
2020-02-12 20:48:03 +0100

    Filter devices based on the command line argument

--- main_oneapi.cc
+++ main_oneapi.cc
@@ -14,66 +14,82 @@ namespace {
   constexpr int NLOOPS = 100;
 }
 
-enum DeviceType { default_device = 0, host_device, cpu_device, gpu_device, cuda_device };
+enum class DeviceType { all_devices = -1, default_device = 0, host_device, cpu_device, gpu_device, cuda_device };
 
 class cuda_selector : public cl::sycl::device_selector {
 public:
   int operator()(const cl::sycl::device & device) const override {
-    using namespace cl::sycl::info;
     std::string const& name = device.get_info<cl::sycl::info::device::name>();
     std::string const& vendor = device.get_info<cl::sycl::info::device::vendor>();
     if (device.is_gpu() and (name.find("NVIDIA") != std::string::npos or vendor.find("NVIDIA") != std::string::npos)) {
-      return 1;
+      return 1000;
     };
     return -1;
   }
 };
 
+template <typename T>
+cl::sycl::vector_class<cl::sycl::device> get_devices(T selector) {
+  auto const& all_devices = cl::sycl::device::get_devices();
+  cl::sycl::vector_class<cl::sycl::device> devices;
+  auto size = std::count_if(all_devices.begin(), all_devices.end(), [selector](cl::sycl::device device){ return selector(device) > 0; });
+  devices.reserve(size);
+  std::copy_if(all_devices.begin(), all_devices.end(), std::back_inserter(devices), [selector](cl::sycl::device device){ return selector(device) > 0; });
+  return devices;
+}
+
 int main(int argc, char **argv) {
-  /*
-  DeviceType device_type = default_device;
+  DeviceType device_type = DeviceType::all_devices;
   if (argc > 1) {
-    if (std::strcmp(argv[1], "--host") == 0)
-      device_type = host_device;
+    if (std::strcmp(argv[1], "--all") == 0)
+      device_type = DeviceType::all_devices;
+    else if (std::strcmp(argv[1], "--default") == 0)
+      device_type = DeviceType::default_device;
+    else if (std::strcmp(argv[1], "--host") == 0)
+      device_type = DeviceType::host_device;
     else if (std::strcmp(argv[1], "--cpu") == 0)
-      device_type = cpu_device;
+      device_type = DeviceType::cpu_device;
     else if (std::strcmp(argv[1], "--gpu") == 0)
-      device_type = gpu_device;
+      device_type = DeviceType::gpu_device;
     else if (std::strcmp(argv[1], "--cuda") == 0)
-      device_type = cuda_device;
+      device_type = DeviceType::cuda_device;
     else
       std::cout << "Ignoring unknown option " << argv[1] << std::endl;
   }
 
-  cl::sycl::device device;
+  cl::sycl::vector_class<cl::sycl::device> devices;
+
   switch (device_type) {
-    case host_device: {
-      cl::sycl::host_selector selector;
-      device = cl::sycl::device{selector};
+    case DeviceType::all_devices: {
+      devices = cl::sycl::device::get_devices();
       break;
     }
-    case cpu_device: {
-      cl::sycl::cpu_selector selector;
-      device = cl::sycl::device{selector};
-      break;
-    }
-    case gpu_device: {
-      cl::sycl::gpu_selector selector;
-      device = cl::sycl::device{selector};
-      break;
-    }
-    case cuda_device: {
-      cuda_selector selector;
-      device = cl::sycl::device{selector};
-      break;
-    }
-    case default_device:
-    default: {
+    case DeviceType::default_device: {
       cl::sycl::default_selector selector;
-      device = cl::sycl::device{selector};
+      devices.push_back(cl::sycl::device{selector});
+      break;
+    }
+    case DeviceType::host_device: {
+      cl::sycl::host_selector selector;
+      devices.push_back(cl::sycl::device{selector});
+      break;
+    }
+    case DeviceType::cpu_device: {
+      cl::sycl::cpu_selector selector;
+      devices = ::get_devices(selector);
+      break;
+    }
+    case DeviceType::gpu_device: {
+      cl::sycl::gpu_selector selector;
+      devices = ::get_devices(selector);
+      break;
+    }
+    case DeviceType::cuda_device: {
+      cuda_selector selector;
+      devices = ::get_devices(selector);
+      break;
     }
   }
-  */
 
   Input input = read_input();
   std::cout << "Got " << input.cablingMap.size << " for cabling, wordCounter " << input.wordCounter << std::endl;
@@ -81,7 +97,6 @@ int main(int argc, char **argv) {
   std::unique_ptr<Output> output = std::make_unique<Output>();
   double totaltime = 0;
 
-  auto devices = cl::sycl::device::get_devices();
   for (auto & device: devices) {
     std::cout << std::endl;
     std::cout << "Running on SYCL device " << device.get_info<cl::sycl::info::device::name>() << ", driver version "
