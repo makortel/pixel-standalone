29bbd32c50249f15dd1aee746298fa7d959fab0e
Matti Kortelainen <matti.kortelainen@cern.ch>
2020-02-28 12:52:21 -0600

    Remove ifdefs from analyze

--- analyzer_kokkos.cc
+++ analyzer_kokkos.cc
@@ -27,43 +27,35 @@ namespace kokkos {
       output = Output();
       const auto wordCounter = input.wordCounter;
 
-#ifdef DIGI_KOKKOS_CUDA
       // Rather non-idiomatic use of Kokkos::View...
-      Kokkos::View<Input, Kokkos::CudaSpace> input_d{"input_d"};
-      Kokkos::View<Input, Kokkos::CudaSpace>::HostMirror input_h = Kokkos::create_mirror_view(input_d);
+      Kokkos::View<Input, KokkosExecSpace> input_d{"input_d"};
+      Kokkos::View<Input, KokkosExecSpace>::HostMirror input_h = Kokkos::create_mirror_view(input_d);
       std::memcpy(input_h.data(), &input, sizeof(Input));
 
-      Kokkos::View<Output, Kokkos::CudaSpace> output_d{"output_d"};
-      Kokkos::View<Output, Kokkos::CudaSpace>::HostMirror output_h = Kokkos::create_mirror_view(output_d);
-      output_h(0).err.construct(pixelgpudetails::MAX_FED_WORDS, output_d.data()->err_d);
-#else   // not DIGI_KOKKOS_CUDA \
-        // Rather non-idiomatic use of Kokkos::View...
-      Kokkos::View<const Input, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> > input_d{&input};
-      Kokkos::View<Output, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> > output_d{&output};
-#endif  // DIGI_KOKKOS_CUDA
+      Kokkos::View<Output, KokkosExecSpace> output_d{"output_d"};
+      Kokkos::View<Output, KokkosExecSpace>::HostMirror output_h = Kokkos::create_mirror_view(output_d);
+      output_h.data()->err.construct(pixelgpudetails::MAX_FED_WORDS, output_d.data()->err_d);
+
+      // could I still use unmanaged somehow to avoid the memcpy above and below?
+      //Kokkos::View<const Input, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> > input_d{&input};
+      //Kokkos::View<Output, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged> > output_d{&output};
 
       auto start = std::chrono::high_resolution_clock::now();
-#ifdef DIGI_KOKKOS_CUDA
       Kokkos::deep_copy(input_d, input_h);
-#endif  // DIGI_KOKKOS_CUDA
 
       Kokkos::parallel_for(
           Kokkos::RangePolicy<KokkosExecSpace>(0, input.wordCounter), KOKKOS_LAMBDA(const size_t i) {
             kokkos::rawtodigi(input_d.data(), output_d.data(), wordCounter, true, true, false, i);
           });
-#ifdef DIGI_KOKKOS_CUDA
       Kokkos::fence();  // I don't know if parallel_for is synchronous or not
       Kokkos::deep_copy(output_h, output_d);
-#endif  // DIGI_KOKKOS_CUDA
       Kokkos::fence();
 
       auto stop = std::chrono::high_resolution_clock::now();
 
-#ifdef DIGI_KOKKOS_CUDA
-      output_h(0).err.set_data(output_h(0).err_d);
-      std::memcpy(output.get(), output_h.data(), sizeof(Output));
-      output->err.set_data(output->err_d);
-#endif  // DIGI_KOKKOS_CUDA
+      output_h.data()->err.set_data(output_h.data()->err_d);
+      std::memcpy(&output, output_h.data(), sizeof(Output));
+      output.err.set_data(output.err_d);
 
       auto diff = stop - start;
       auto time = std::chrono::duration_cast<std::chrono::microseconds>(diff).count();
